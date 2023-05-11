ce8424af246bcec4479859f67b1750db8b968b18
Shixiong Shang <shixiong.shang@sysdig.com>
2023-05-11 12:13:47 -0400

    Initial commit

--- operator/redisfailover/config.go
+++
@@ -1,8 +0,0 @@
-package redisfailover
-
-// Config is the configuration for the redis operator.
-type Config struct {
-	ListenAddress string
-	MetricsPath   string
-	Concurrency   int
-}
--- operator/redisfailover/service/constants_test.go
+++
@@ -1,8 +0,0 @@
-package service_test
-
-const (
-	name         = "test"
-	namespace    = "testns"
-	sentinelName = "rfs-test"
-	redisName    = "rfr-test"
-)
